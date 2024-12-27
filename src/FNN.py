ca8d295a2220978bea453547ddfae0ec1373951a
Cayden Bernard <caydenbernard@live.com>
2024-12-27 13:54:27 -0500

    feedforward update and starting training implementation

--- src/FNN.py
+++ src/FNN.py
@@ -35,10 +35,14 @@ class FNN():
         return np.maximum(x, 0)
     
     def FeedForward(self):
-        for i in range(self.num_hidden + 1):
-            buffer = np.dot(self.weights[i], self.layers[i])
-            self.layers[i+1] = self.ReLU(buffer + self.biases[i])
+        for i in range(self.num_hidden):
+            self.layers[i+1] = self.ReLU(np.dot(self.weights[i], self.layers[i]) + self.biases[i])
+        self.layers[-1] = self.SoftMax(np.dot(self.weights[-1], self.layers[-1]) + self.biases[-1])
 
-        self.layers[-1] = self.SoftMax(self.layers[-1])
+class Training():
+    def __init__(self, FNN, learning_rate = 0.01):
+        self.FNN = FNNs
+        self.learning_rate = learning_rate
 
-        
+    def dReLU(x):
+        return np.where(x > 0, 1, 0)
